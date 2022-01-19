2a3367df1adf8b6beae8cab1936c4c6645f8f3ac
namanr17 <namanraj@cse.iitk.ac.in>
2022-01-19 17:07:30 +0530

    Updates

---
+++ precache-manifest.cf38aa93e521929bcc4c65ad65bbb266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9198ae3b868f769fc8ea",
+    "url": "/pics/static/js/runtime~main.7218180c.js"
+  },
+  {
+    "revision": "95b01861be8f53092b50",
+    "url": "/pics/static/js/main.6ddf65c6.chunk.js"
+  },
+  {
+    "revision": "227c99dae24524327f86",
+    "url": "/pics/static/js/2.9b5a5c39.chunk.js"
+  },
+  {
+    "revision": "95b01861be8f53092b50",
+    "url": "/pics/static/css/main.8e10935f.chunk.css"
+  },
+  {
+    "revision": "c0f3e319a4eff6a85cde1e7493c7555c",
+    "url": "/pics/index.html"
+  }
+];
